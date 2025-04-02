985b507d2c9a74b0335358dd3675126a4f58a778
david muchoki <dmmuchoki7@gmail.com>
2025-04-03 00:47:11 +0300

    Add user authentication and management routes to URLs

--- hospital_appointment/users/urls.py
+++ hospital_appointment/users/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
-
+from . import views
 urlpatterns = [
+    path('noexist/callback/', views.Redirect.as_view()),
+    path('register', views.RegisterUser.as_view(), name='register'),
+    path('login', views.Login.as_view(), name='login'),
+    path('logout', views.Logout.as_view(), name='logout'),
     
+    path('user/', views.UserList.as_view()),
 ]
