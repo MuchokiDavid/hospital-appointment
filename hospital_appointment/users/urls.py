eb9c7886306a60423a977be1ad1143d34d28e194
david muchoki <dmmuchoki7@gmail.com>
2025-04-03 17:40:05 +0300

    Add endpoints for specialization and doctor profile management

--- hospital_appointment/users/urls.py
+++ hospital_appointment/users/urls.py
@@ -5,6 +5,10 @@ urlpatterns = [
     path('register', views.RegisterUser.as_view(), name='register'),
     path('login', views.Login.as_view(), name='login'),
     path('logout', views.Logout.as_view(), name='logout'),
+    path('post-specialization/', views.PostSpecializationView.as_view(), name='post specialisation'),
+    path('get-specialization/', views.GetSpecializationsView.as_view(), name='get specialisation'),
+    
+    path('doctor-profile/', views.DoctorProfileView.as_view(), name='doctor profile'),
     
     path('user/', views.UserList.as_view()),
 ]
