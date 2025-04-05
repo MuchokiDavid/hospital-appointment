34d16838795534f830bb88b8cdb0a21bd234db82
david muchoki <dmmuchoki7@gmail.com>
2025-04-05 21:01:24 +0300

    Add new endpoints for doctor availability, time off, appointments, medical records, and notifications

--- hospital_appointment/appointmentapp/urls.py
+++ hospital_appointment/appointmentapp/urls.py
@@ -2,17 +2,32 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ---<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Doctor availability schedule>>>>>>>>>>>>>>>>>>>>>>--------------------
     path('doctor-availability-schedule/', views.AvailabilityScheduleView.as_view(), name='availability-schedule'), #GET, POST Authenticate: Doctor user
+    path('availability-schedule-by-id/<int:id>/', views.AvailabilityScheduleByIdView.as_view(), name='update-availability-schedule'), #PUT, DELETE Authenticate: Doctor user
     path('all-availability-schedule/', views.GetAllAvailabilityScheduleView.as_view(), name='all-availability-schedule'), #GET, POST Authenticate user
-    path('update-availability-schedule/<int:id>/', views.AvailabilityScheduleUpdateView.as_view(), name='update-availability-schedule'), #PUT, DELETE Authenticate: Doctor user
     
-    path('doctor-time-off/', views.TimeOffView.as_view(), name='time-off'), #GET, POST Authenticate: Doctor user
+    # ---<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Doctor time off schedule>>>>>>>>>>>>>>>>>>>>>>--------------------
+    path('time-off/', views.TimeOffView.as_view(), name='time-off'), #GET, POST Authenticate: Doctor user
+    path('time-off-by-id/<int:id>/', views.TimeOffByIdView.as_view(), name='update-time-off'), #PUT, DELETE Authenticate: Doctor user
     path('all-time-off/', views.GetAllTimeOffView.as_view(), name='all-time-off'), #GET, POST Authenticate user
-    path('update-time-off/<int:id>/', views.TimeOffUpdateView.as_view(), name='update-time-off'), #PUT, DELETE Authenticate: Doctor user
     
+    # ---<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Appointment>>>>>>>>>>>>>>>>>>>>>>--------------------
     path('appointment/', views.AppointmentView.as_view(), name='appointment'), #GET, POST Authenticate: Doctor user
-    path('update-appointment/<int:id>/', views.AppointmentUpdateView.as_view(), name='update-appointment'), #PUT, DELETE Authenticate: Doctor user
-    # path('all-appointment/', views.GetAllAppointmentView.as_view(), name='all-appointment'), #GET, POST Authenticate user
+    path('appointment-by-id/<int:id>/', views.AppointmentByIdView.as_view(), name='update-appointment'), #PUT, DELETE Authenticate: Doctor user
+    path('all-appointment/', views.GetAllAppointmentView.as_view(), name='all-appointment'), #GET, POST Authenticate user
     
+    # ----<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Medical Record>>>>>>>>>>>>>>>>>>>>>>--------------------
+    path('medical-record/', views.MedicalRecordView.as_view(), name='medical-record'), #GET, POST Authenticate: Doctor user
+    path('medical-record-by-id/<int:id>/', views.MedicalRecordByIdView.as_view(), name='update-medical-record'), #PUT, DELETE Authenticate: Doctor user
+    path('all-medical-record/', views.GetAllMedicalRecordView.as_view(), name='all-medical-record'), #GET, POST Authenticate user
+    
+    # ----<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Prescription>>>>>>>>>>>>>>>>>>>>>>--------------------
+    # path('prescription/', views.PrescriptionView.as_view(), name='prescription'), #GET, POST Authenticate: Doctor user
+    # path('update-prescription/<int:id>/', views.PrescriptionUpdateView.as_view(), name='update-prescription'), #PUT, DELETE Authenticate: Doctor user
+    # path('all-prescription/', views.GetAllPrescriptionView.as_view(), name='all-prescription'), #GET, POST Authenticate user
+    
+    # ------<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Notification>>>>>>>>>>>>>>>>>>>>>>--------------------
     path('notification/', views.GetNotifications.as_view(), name='notification'), #GET Authenticate: Doctor user
+    path('notification-by-id/<int:id>/', views.NotificationById.as_view(), name='update-notification'), #PUT, DELETE Authenticate: Doctor user
 ]
